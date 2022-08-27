7637a95b8354c84557298cf2d94f02854cbaeea7
Liusch <zhevila13@yandex.ru>
2022-08-27 22:39:45 +0500

    f-str

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+stroka = 'hi - \"'
+
+chislo = 5
+
+test = stroka + str(chislo) + '\" символ обратного слэша \\ это экранирование'
+
+print(test)
+
+print('Если поставить \\n то \n начнётся новая строка')
+
+print('Если поставить \\t то \t будет отступ (табуляция)')
+
+# f-строки
+print(f'{stroka}{chislo}" можно и так без преобразования в str')
+'''
+
+number = input('Введите число от 1 до 5: ')
+print(f'Вы ввели число - {number}')
+
+#Подключение модуля random
+
+import random
+numberAI = random.randint(1,5)
+
+print(f'а число загаданное компьютером: {numberAI}')
+
+if (number == numberAI):
+    print('победа')
+
